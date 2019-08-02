e7a316aea5e1c929d02b16558ba63073379dbb87
PSEH-4 <PSEH-4>
2019-08-02 16:33:24 +0530

    Updated startServer script

--- startServer.sh
+++ startServer.sh
@@ -1,10 +1,13 @@
 #/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
-nvm install --lts=Dubnium
-nvm use lts/dubnium
-npm install
+NODE_CHECK=`which node`
+if [ -z "$NODE_CHECK" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  source ~/.bashrc
+  nvm install --lts=Dubnium
+  nvm use lts/dubnium
+  npm install
+fi
 
 sudo chown -R $USER:$GROUP ~/.npm
 sudo chown -R $USER:$GROUP ~/.config
