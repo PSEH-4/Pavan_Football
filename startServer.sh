1d328efd9abd6764a136a1aad1226cc31dacdb5e
PSEH-4 <PSEH-4>
2019-08-02 16:28:22 +0530

    Updated startServer script

--- startServer.sh
+++ startServer.sh
@@ -10,7 +10,7 @@ sudo chown -R $USER:$GROUP ~/.npm
 sudo chown -R $USER:$GROUP ~/.config
 
 PID=`ps | grep "node bin/www" | head -n 1 | awk '{print $1}'`
-if [[ ! -z "$PID"]]; then
+if [[ ! -z "$PID" ]]; then
   kill -2 $PID
 fi
 nohup node bin/www & > nohup.out
